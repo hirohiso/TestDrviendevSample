6c24f8c0e03ef06321b9780313b6bd9bda60b3a1
Irie HIronori <irie.h126@gmail.com>
2019-04-29 17:22:48 +0900

    add 新規作成
    1部第1章の途中まで

---
+++ src/main/java/hirohiso/SampleTest/Dollar.java
@@ -0,0 +1,16 @@
+package hirohiso.SampleTest;
+
+public class Dollar {
+
+	public int amount;
+
+	public Dollar(int amount) {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public void times(int i) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ src/test/java/hirohiso/SampleTest/MoneyTest.java
@@ -0,0 +1,17 @@
+package hirohiso.SampleTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MoneyTest {
+
+	@Test
+	public void test() {
+		Dollar five = new Dollar(5);
+		five.times( 2);
+		assertEquals(10, five.amount);
+
+	}
+
+}
